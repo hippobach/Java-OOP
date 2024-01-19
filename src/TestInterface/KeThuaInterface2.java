e080d0274b38ed79e4bea8308bf4a5a36fffbd9d
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-01-19 09:24:31 +0700

    interface in java oop

---
+++ src/TestInterface/KeThuaInterface2.java
@@ -0,0 +1,18 @@
+package TestInterface;
+
+public class KeThuaInterface2 implements ViDuInterface, ViDuInterface2{
+    @Override
+    public void thongTin(String ten, String cccd, int namSinh) {
+
+    }
+
+    @Override
+    public double tinhLuong(double luongNgay, int soNgayCong) {
+        return luongNgay*soNgayCong;
+    }
+
+    @Override
+    public void xetThuong(int cccd, double doanhThu) {
+
+    }
+}
---
+++ src/TestInterface/KeThuaInterface3.java
@@ -0,0 +1,23 @@
+package TestInterface;
+
+public class KeThuaInterface3 implements ViDuInterface3{
+    @Override
+    public void thongTin(String ten, String cccd, int namSinh) {
+
+    }
+
+    @Override
+    public double tinhLuong(double luongNgay, int soNgayCong) {
+        return luongNgay*soNgayCong;
+    }
+
+    @Override
+    public void xetThuong(int cccd, double doanhThu) {
+
+    }
+
+    @Override
+    public void X(String x) {
+
+    }
+}
