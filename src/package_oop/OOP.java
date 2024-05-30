01201fa3fa4581136fcba636266fbb680cf33132
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-05-30 10:04:16 +0700

    property and method in oop

--- src/package_oop/OOP.java
+++ src/package_oop/OOP.java
@@ -7,6 +7,14 @@ public class OOP {
         Student studentOne = new Student();
         Student studentTwo = new Student();
 
+        studentOne.birthday = 1994;
+        studentOne.name = "John Shelby";
+        studentOne.code = "S01";
+
+        System.out.println("Birthday: "+studentOne.birthday);
+        System.out.println("Name: "+studentOne.name);
+        System.out.println("Code: "+studentOne.code);
+
         Teacher teacherOne = new Teacher();
         Teacher teacherTwo = new Teacher();
     }
