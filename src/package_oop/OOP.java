f84eaea270ef5e69c3835b15cefb3816eb0a3273
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-05-31 21:52:03 +0700

    constructor in oop

--- src/package_oop/OOP.java
+++ src/package_oop/OOP.java
@@ -4,13 +4,13 @@ public class OOP {
     public static void main(String[] args) {
         System.out.println("OOP Programming");
 
-        Student studentOne = new Student();
+        Student studentOne = new Student("John", "SV01");
 
-        studentOne.setBirthday(1994);
-        studentOne.setName("John Shelby");
+//        studentOne.setBirthday(1994);
+//        studentOne.setName("John Shelby");
 //        studentOne.setCode("S01");
 //        studentOne.setCode();
-        studentOne.setCode("SV", 213);
+//        studentOne.setCode("SV", 213);
         studentOne.showInfo();
     }
 }
