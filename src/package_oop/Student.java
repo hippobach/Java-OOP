1540fbe6c51e8d2205acd21322b8f710249c5628
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-05-31 21:10:26 +0700

    getters andd setters in oop

--- src/package_oop/Student.java
+++ src/package_oop/Student.java
@@ -1,7 +1,48 @@
 package package_oop;
 
+import java.util.Calendar;
+
 public class Student {
     public String name;
     public String code;
     public int birthday;
+
+//    getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+//    Show student info
+    public void showInfo() {
+        System.out.println("Birthday: "+this.getBirthday());
+        System.out.println("Name: "+this.getName());
+        System.out.println("Code: "+this.getCode());
+        System.out.println("Age: "+this.getAge());
+    }
+
+//    Calculate the age
+    public int getAge() {
+        Calendar now = Calendar.getInstance();
+        return now.get(Calendar.YEAR) - this.getBirthday();
+    }
 }
